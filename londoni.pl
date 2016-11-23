723c4108e8f2f1e83e8737f207e1e96b4d6f206f
Rulqu <jari@vilkas.fi>
2016-11-23 16:40:17 +0200

    This should work

--- londoni.pl
+++ londoni.pl
@@ -6,23 +6,26 @@ use vars qw($VERSION %IRSSI);
 use Irssi;
 $VERSION = '1.00';
 %IRSSI = (
-    authors     => 'Perttu Rautaniemi',
-    contact     => 'author@far.away',
-    name        => 'My First Script',
-    description => 'This script allows ' .
-                   'you to print words characters ' .
-                   'separated to multiple lines ',
-    license     => 'GPL',
+    authors     =>  'Perttu Rautaniemi',
+    contact     =>  'author@far.away',
+    name        =>  'My First Script',
+    description =>  'This script allows ' .
+                    'you to print words characters ' .
+                    'separated to multiple lines ',
+    license     =>  'GPL',
 );
 sub lontoo {
     my ($data, $server, $witem) = @_;
     return unless $witem;
-	my @string = split undef, $data;
-	for(my $i = 0; $i < @string.length; $i++){
-        	if ($i == 0){$witem->command("MSG ".$witem->{name}." @string");}
-        	else{$witem->command("MSG ".$witem->{name}." @string[$i]");}
-	}
-
+    my @string = split //, decode("utf-8",$data);
+    for(my $i = 0; $i < @string.length; $i++) {
+        if ($i == 0) {
+            $witem->command("MSG ".$witem->{name}." @string");
+        }
+        else {
+            $witem->command("MSG ".$witem->{name}." @string[$i]");
+        }
+    }
 }
 
 Irssi::command_bind londoni => \&lontoo;
